13ac328e1833c78d8eadc980262da918032b21b3
Jose Garcia <josegarcia19809@gmail.com>
2025-08-31 15:55:08 -0600

    Desarrollo web con Spring Framework y MySQL8: Anotación @RequestParam - Ejemplo Petición HTTP GET

--- empleos/src/main/java/com/example/empleos/controllers/VacanteController.java
+++ empleos/src/main/java/com/example/empleos/controllers/VacanteController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,6 +18,13 @@ import java.util.List;
 @Controller
 @RequestMapping("/vacantes")
 public class VacanteController {
+
+    @GetMapping("/delete")
+    public String eliminar(@RequestParam("id") int idVacante, Model model) {
+        System.out.println("Borrando vacante con id: " + idVacante);
+        model.addAttribute("idVacante", idVacante);
+        return "vacantes/mensaje";
+    }
 
     @GetMapping("/view/{id}")
     public String verDetalle(@PathVariable("id") int idVacante, Model model) {
