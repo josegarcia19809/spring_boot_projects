fecc69e31d7604e4660e71581854eac7d9427145
Jose Garcia <josegarcia19809@gmail.com>
2025-08-31 12:49:47 -0600

    Desarrollo web con Spring Framework y MySQL8: Anotación @RequestMapping a nivel de un método

---
+++ empleos/src/main/java/com/example/empleos/controllers/CategoriasController.java
@@ -0,0 +1,28 @@
+package com.example.empleos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CategoriasController {
+    // @GetMapping("/index")
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String mostrarIndex(Model model){
+        return "categorias/listaCategorias";
+    }
+
+    // @GetMapping("/create")
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String crear(Model model){
+        return "categorias/formCategoria";
+    }
+
+    // @PostMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String guardar(Model model){
+        return "categorias/listaCategorias";
+    }
+}
+
