1154ca7eadc71da9f79a68af2e310cc9fff950db
Jose Garcia <josegarcia19809@gmail.com>
2025-09-07 14:28:52 -0600

    Proyecto Refacciones: Iniciando el proyecto

---
+++ refacciones/src/main/java/com/josegarcia/refacciones/RefaccionesApplication.java
@@ -0,0 +1,14 @@
+package com.josegarcia.refacciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefaccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefaccionesApplication.class, args);
+        System.out.println("RefaccionesApplication started...");
+	}
+
+}
---
+++ refacciones/src/test/java/com/josegarcia/refacciones/RefaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josegarcia.refacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
