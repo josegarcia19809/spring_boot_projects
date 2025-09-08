ee39ae2240d9042eb12612f36531548c4ebda595
Jose Garcia <josegarcia19809@gmail.com>
2025-09-07 19:19:12 -0600

    Proyecto Refacciones: Anotación @RequestParam - Ejemplo Petición HTTP GET

--- refacciones/src/main/java/com/josegarcia/refacciones/controllers/RefaccionController.java
+++ refacciones/src/main/java/com/josegarcia/refacciones/controllers/RefaccionController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,14 @@ public class RefaccionController {
         model.addAttribute("idRefaccion", idRefaccion);
         return "detalle";
     }
+
+    @GetMapping("/delete")
+    public String eliminar(@RequestParam("id") int idRefaccion, Model model) {
+        System.out.println("Borrando refacción con id: " + idRefaccion);
+        model.addAttribute("idRefaccion", idRefaccion);
+        return "borrar";
+    }
+
 
 
     // Método que llena un ArrayList con objetos Refaccion (demo)
