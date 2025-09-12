13a102a4d07f2560c1c25e43b15ccce3e059498f
Jose Garcia <josegarcia19809@gmail.com>
2025-09-11 18:58:47 -0600

    Proyecto Refacciones: Clase de Servicio RefaccionServiceImpl

---
+++ refacciones/src/main/java/com/josegarcia/refacciones/service/IRefaccionService.java
@@ -0,0 +1,9 @@
+package com.josegarcia.refacciones.service;
+
+import com.josegarcia.refacciones.models.Refaccion;
+
+import java.util.List;
+
+public interface IRefaccionService {
+    List<Refaccion> buscarTodas();
+}
---
+++ refacciones/src/main/java/com/josegarcia/refacciones/service/RefaccionServiceImpl.java
@@ -0,0 +1,23 @@
+package com.josegarcia.refacciones.service;
+
+import com.josegarcia.refacciones.models.Refaccion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RefaccionServiceImpl implements IRefaccionService {
+    List<Refaccion> listaRefacciones = null;
+
+    public RefaccionServiceImpl() {
+        listaRefacciones = new ArrayList<>();
+        listaRefacciones.add(new Refaccion(1, "Filtro de aceite", "Motor", 2023, 120.50f));
+        listaRefacciones.add(new Refaccion(2, "Bujía iridium", "Encendido", 2022, 95.00f));
+        listaRefacciones.add(new Refaccion(3, "Pastillas de freno", "Frenos", 2024, 310.99f));
+        listaRefacciones.add(new Refaccion(4, "Amortiguador trasero", "Suspensión", 2021, 850.00f));
+    }
+
+    @Override
+    public List<Refaccion> buscarTodas() {
+        return listaRefacciones;
+    }
+}
