87ed9023f2667aba8e1cd6e6c8b8f9089ea0c012
Jose Garcia <josegarcia19809@gmail.com>
2025-09-15 10:34:44 -0600

    Spring Boot. DataVault prototipo. Creación de proyecto

---
+++ dataVault_v1/src/main/java/com/example/dataVault_v1/DataVaultV1Application.java
@@ -0,0 +1,13 @@
+package com.example.dataVault_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVaultV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataVaultV1Application.class, args);
+	}
+
+}
---
+++ dataVault_v1/src/test/java/com/example/dataVault_v1/DataVaultV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataVault_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVaultV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
