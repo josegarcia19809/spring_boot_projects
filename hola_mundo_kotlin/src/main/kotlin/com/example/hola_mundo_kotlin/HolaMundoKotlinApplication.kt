848c80ca690d1e39521ec71f41267ee307e10d9b
Jose Garcia <josegarcia19809@gmail.com>
2025-09-25 20:31:48 -0600

    Auto stash before merge of "main" and "origin/main"
    Spring Boot Kotlin. Actualización

--- hola_mundo_kotlin/src/main/kotlin/com/example/hola_mundo_kotlin/HolaMundoKotlinApplication.kt
+++ hola_mundo_kotlin/src/main/kotlin/com/example/hola_mundo_kotlin/HolaMundoKotlinApplication.kt
@@ -3,10 +3,14 @@ package com.example.hola_mundo_kotlin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
+
 class HolaMundoKotlinApplication
 
 fun main(args: Array<String>) {
 	runApplication<HolaMundoKotlinApplication>(*args)
+
     println("Ejecutando servidor...")
 }
+
