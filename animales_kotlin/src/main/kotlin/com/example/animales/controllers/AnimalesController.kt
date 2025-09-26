4c7f09c4b813d891b0b61d4883fb0833320e1704
Jose Garcia <josegarcia19809@gmail.com>
2025-09-25 21:51:21 -0600

    Spring Boot kotlin: actualización

--- animales_kotlin/src/main/kotlin/com/example/animales/controllers/AnimalesController.kt
+++ animales_kotlin/src/main/kotlin/com/example/animales/controllers/AnimalesController.kt
@@ -6,13 +6,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AnimalesController {
     @GetMapping("/")
-    fun inicio(): String{
-
+    fun inicio(): String {
         return "<h2>🐻🐭Animales favoritos 🐸🐢</h2>"
     }
 
     @GetMapping("/oso")
-    fun mostrarOso(): String{
+    fun mostrarOso(): String {
         return "<h3>Oso cariñoso 🐻</h3>"
+    }
+
+    @GetMapping("/perro")
+    fun mostrarPerro(): String {
+        return "<h3>Perro Firulais 🐕</h3>"
+    }
+
+    @GetMapping("/gato")
+    fun mostrarGato(): String {
+        return "<h3>Gato Mixi 🐈</h3>"
     }
 }
