bb49cef7f1d6ec4993f4e8c6cbfc3ea76717d998
Jose Garcia <josegarcia19809@gmail.com>
2025-09-27 17:26:37 -0600

    Spring Boot. Bóveda de datos: Creación

---
+++ boveda_datos/src/main/java/com/example/boveda_datos/BovedaDatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.boveda_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BovedaDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BovedaDatosApplication.class, args);
+	}
+
+}
---
+++ boveda_datos/src/test/java/com/example/boveda_datos/BovedaDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boveda_datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovedaDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
