27c0bbb00dbe7d162e34a902b6b23531fb5294a2
Jose Garcia <josegarcia19809@gmail.com>
2025-09-29 18:43:20 -0600

    Desarrollo web con Spring Framework y MySQL8: Integrar el diseño de la página principal dinámicamente (Parte 2)

--- empleos/src/main/java/com/example/empleos/controllers/HomeController.java
+++ empleos/src/main/java/com/example/empleos/controllers/HomeController.java
@@ -1,12 +1,23 @@
 package com.example.empleos.controllers;
 
+import com.example.empleos.models.Vacante;
+import com.example.empleos.service.IVacanteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    IVacanteService vacanteService;
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        List<Vacante> vacantes = vacanteService.buscarTodas();
+        model.addAttribute("vacantes", vacantes);
         return "home";
     }
 }
