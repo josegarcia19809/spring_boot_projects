844903c8355d944f2ca10723ccb67fde29cbabe8
Jose Garcia <josegarcia19809@gmail.com>
2025-10-07 20:56:52 -0600

    Spring Boot 3 - Desarrollo web con Spring Framework y MySQL8: JPA - Interfaz CommandLineRunner

--- jpa_empleos/src/main/java/com/example/jpa_empleos/JpaEmpleosApplication.java
+++ jpa_empleos/src/main/java/com/example/jpa_empleos/JpaEmpleosApplication.java
@@ -1,13 +1,28 @@
 package com.example.jpa_empleos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaEmpleosApplication {
+public class JpaEmpleosApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaEmpleosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpaEmpleosApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        guardar();
+        eliminar();
+    }
+
+    private void guardar() {
+        System.out.println("guardando");
+    }
+
+    private void eliminar() {
+        System.out.println("eliminando");
+
+    }
 }
