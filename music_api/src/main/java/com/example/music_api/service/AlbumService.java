39ccb24c430413491169a629274c0b2f4c92bd1f
Jose Garcia <josegarcia19809@gmail.com>
2025-10-28 21:15:26 -0600

    Spring Boot - Restful Web Services: Crear la clase de Servicio para la entidad Album

---
+++ music_api/src/main/java/com/example/music_api/service/AlbumService.java
@@ -0,0 +1,20 @@
+package com.example.music_api.service;
+
+import com.example.music_api.models.Album;
+import com.example.music_api.repository.AlbumRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AlbumService implements IAlbumService {
+
+    private final AlbumRepository albumRepository;
+
+    @Override
+    public List<Album> buscarTodos() {
+        return albumRepository.findAll();
+    }
+}
---
+++ music_api/src/main/java/com/example/music_api/service/IAlbumService.java
@@ -0,0 +1,9 @@
+package com.example.music_api.service;
+
+import com.example.music_api.models.Album;
+
+import java.util.List;
+
+public interface IAlbumService {
+    List<Album> buscarTodos();
+}
