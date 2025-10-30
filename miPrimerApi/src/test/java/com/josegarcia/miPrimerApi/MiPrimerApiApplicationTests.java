78f7fb2d6792ef34dfb7018aa317d7761abfed70
josegarcia19809 <josegarcia19809@gmail.com>
2025-10-30 12:00:54 -0600

    API REST CAFE: Inicio de proyecto

---
+++ miPrimerApi/src/test/java/com/josegarcia/miPrimerApi/MiPrimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josegarcia.miPrimerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
