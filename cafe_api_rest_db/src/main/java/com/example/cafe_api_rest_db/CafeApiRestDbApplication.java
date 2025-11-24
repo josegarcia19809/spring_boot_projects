c4014ada8bc76fe725bb15d307209407df0d4a01
Jose Garcia <josegarcia19809@gmail.com>
2025-11-24 12:56:36 -0600

    Café API Rest DB MySQL: Creando el proyecto

---
+++ cafe_api_rest_db/src/main/java/com/example/cafe_api_rest_db/CafeApiRestDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafe_api_rest_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeApiRestDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeApiRestDbApplication.class, args);
+	}
+
+}
---
+++ cafe_api_rest_db/src/test/java/com/example/cafe_api_rest_db/CafeApiRestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafe_api_rest_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApiRestDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
