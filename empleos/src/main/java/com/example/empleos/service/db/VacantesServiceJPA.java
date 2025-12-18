be993f8a6f8bf39b7962e70dd6edce4aa2130c04
Jose Garcia <josegarcia19809@gmail.com>
2025-12-18 10:38:11 -0600

    Desarrollo web con Spring Framework y MySQL8: Crear una nueva implementación de IVacantesService con nuestro Repositorio

---
+++ empleos/src/main/java/com/example/empleos/service/db/VacantesServiceJPA.java
@@ -0,0 +1,40 @@
+package com.example.empleos.service.db;
+
+import com.example.empleos.models.Vacante;
+import com.example.empleos.repository.VacantesRepository;
+import com.example.empleos.service.IVacanteService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Primary
+public class VacantesServiceJPA implements IVacanteService {
+
+    private VacantesRepository vacantesRepository;
+
+    public VacantesServiceJPA(VacantesRepository vacantesRepository) {
+        this.vacantesRepository = vacantesRepository;
+    }
+
+    @Override
+    public List<Vacante> buscarTodas() {
+        return vacantesRepository.findAll();
+    }
+
+    @Override
+    public Vacante buscarPorId(Integer idVacante) {
+        Optional<Vacante> vacante = vacantesRepository.findById(idVacante);
+        if (vacante.isPresent()) {
+            return vacante.get();
+        }
+        return null;
+    }
+
+    @Override
+    public void guardar(Vacante vacante) {
+        vacantesRepository.save(vacante);
+    }
+}
