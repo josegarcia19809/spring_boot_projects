511461c78fe807c8c1392e095faa97118fa650c2
Jose Garcia <josegarcia19809@gmail.com>
2025-12-18 09:58:13 -0600

    Desarrollo web con Spring Framework y MySQL8: Anotación @Primary

--- empleos/src/main/java/com/example/empleos/service/db/CategoriasServiceJPA.java
+++ empleos/src/main/java/com/example/empleos/service/db/CategoriasServiceJPA.java
@@ -1,19 +1,22 @@
 package com.example.empleos.service.db;
 
 import com.example.empleos.models.Categoria;
+import com.example.empleos.repository.CategoriasJPARepository;
 import com.example.empleos.repository.CategoriasRepository;
 import com.example.empleos.service.ICategoriasService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Primary
 public class CategoriasServiceJPA implements ICategoriasService {
 
-    private CategoriasRepository categoriasRepository;
+    private CategoriasJPARepository categoriasRepository;
 
-    public CategoriasServiceJPA(CategoriasRepository categoriasRepository) {
+    public CategoriasServiceJPA(CategoriasJPARepository categoriasRepository) {
         this.categoriasRepository = categoriasRepository;
     }
 
@@ -24,8 +27,7 @@ public class CategoriasServiceJPA implements ICategoriasService {
 
     @Override
     public List<Categoria> buscarTodas() {
-        //return ((List<Categoria>) categoriasRepository).findAll();
-        return List.of();
+        return categoriasRepository.findAll();
     }
 
     @Override
