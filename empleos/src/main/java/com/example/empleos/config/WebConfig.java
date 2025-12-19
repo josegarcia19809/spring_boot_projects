7a768fbbff9ab3e0b8e8ac21561612e0a9f9b8bf
Jose Garcia <josegarcia19809@gmail.com>
2025-12-18 20:43:32 -0600

    Desarrollo web con Spring Framework y MySQL8: Configurar recursos estáticos de rutas adicionales.

---
+++ empleos/src/main/java/com/example/empleos/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.empleos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${empleosapp.ruta.imagenes}")
+    private String rutaImagenes;
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/logos/**").addResourceLocations("file:/Users" +
+//                "/josegarcia/empleos/img-vacantes/");
+        registry.addResourceHandler("/img/**").addResourceLocations("file:" + rutaImagenes);
+    }
+}
