b4165ccc6b01bd802077274287c876599df7eb70
Jose Garcia <josegarcia19809@gmail.com>
2025-12-21 08:03:05 -0600

    Desarrollo web con Spring Framework y MySQL8: Personalizar accesos a URLs

--- empleos/src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
+++ empleos/src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
@@ -2,9 +2,12 @@ package com.example.empleos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -12,7 +15,7 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class DatabaseWebSecurity {
 
-    //@Bean
+//    @Bean
 //    UserDetailsManager users(DataSource dataSource) {
 //
 //        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
@@ -31,4 +34,28 @@ public class DatabaseWebSecurity {
                 "perfiles p on p.id = up.id_perfil " + "where u.username=?");
         return users;
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        // Los recursos estáticos no requieren autenticación
+                        .requestMatchers("/bootstrap/**", "/images/**", "/tinymce/**",
+                                "/logos/**").permitAll()
+
+                        // Las vistas públicas no requieren autenticación
+                        .requestMatchers("/", "/signup", "/search",
+                                "/vacantes/view/**").permitAll()
+
+                        // Todas las demás URLs de la Aplicación requieren autenticación
+                        .anyRequest().authenticated()
+                )
+                // El formulario de Login no requiere autenticacion
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                );
+
+        return http.build();
+    }
+
 }
