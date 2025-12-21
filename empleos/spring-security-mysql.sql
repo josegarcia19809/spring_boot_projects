4de6addd61332091cf72c884f772ea237cd5db39
Jose Garcia <josegarcia19809@gmail.com>
2025-12-20 20:42:37 -0600

    Desarrollo web con Spring Framework y MySQL8: Creación de la base de datos por defecto de usuarios de Spring Security

---
+++ empleos/spring-security-mysql.sql
@@ -0,0 +1,25 @@
+-- Crear tabla de usuarios
+CREATE TABLE `users` (
+	`username` varchar(50) NOT NULL,
+	`password` varchar(50) NOT NULL,
+	`enabled` tinyint(1) NOT NULL,
+	PRIMARY KEY (`username`)
+) ENGINE=InnoDB;
+
+-- Crear tabla de roles
+CREATE TABLE `authorities` (
+	`username` varchar(50) NOT NULL,
+	`authority` varchar(50) NOT NULL,
+	UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+	CONSTRAINT `authorities_ibfk_1`
+	FOREIGN KEY (`username`)
+	REFERENCES `users` (`username`)
+) ENGINE=InnoDB;
+
+-- Insertamos nuestros usuarios
+INSERT INTO `users` VALUES ('jgarcia','{noop}master',1);
+INSERT INTO `users` VALUES ('marisol','{noop}mari123',1);
+
+-- Insertamos (asignamos roles) a nuestros usuarios.
+INSERT INTO `authorities` VALUES ('jgarcia','SUPERVISOR');
+INSERT INTO `authorities` VALUES ('marisol','ADMINISTRADOR');
