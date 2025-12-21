c6a52d3e722484a5b50bd7e2fe0e4195dab1d727
Jose Garcia <josegarcia19809@gmail.com>
2025-12-21 07:14:42 -0600

    Desarrollo web con Spring Framework y MySQL8: Recuperar usuarios y roles de la base de datos por defecto

---
+++ empleos/src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
@@ -0,0 +1,23 @@
+package com.example.empleos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class DatabaseWebSecurity {
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        return users;
+
+    }
+
+}
