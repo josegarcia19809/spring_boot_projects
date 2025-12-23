d324b20bba9e54f99df607091a255131eb37a96c
Jose Garcia <josegarcia19809@gmail.com>
2025-12-23 07:22:00 -0600

    Desarrollo web con Spring Framework y MySQL8: Configurar la vista del formulario para enviar una solicitud de vacante

--- empleos/src/main/java/com/example/empleos/controllers/SolicitudesController.java
+++ empleos/src/main/java/com/example/empleos/controllers/SolicitudesController.java
@@ -1,6 +1,9 @@
 package com.example.empleos.controllers;
 
 
+import com.example.empleos.models.Vacante;
+import com.example.empleos.service.IVacanteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/solicitudes")
 public class SolicitudesController {
+
+    @Autowired
+    private IVacanteService vacanteService;
+
     @GetMapping("/create/{idVacante}")
-    public String create(@PathVariable("idVacante") Long idVacante, Model model) {
-        System.out.println("IdVacante: " + idVacante);
+    public String create(@PathVariable("idVacante") Integer idVacante, Model model) {
+        Vacante vacante = vacanteService.buscarPorId(idVacante);
+        model.addAttribute("vacante", vacante);
         return "solicitudes/formSolicitud";
     }
 }
