62dfb32d81e8179a91c9eae4f9b998dfb3591940
Jose Garcia <josegarcia19809@gmail.com>
2026-01-01 11:59:07 -0600

    API REST MySQL & Security Producto: Añadiendo el componente Service

--- api_rest_crud/src/main/java/com/example/api_rest_crud/services/ProductoServiceI.java
+++ api_rest_crud/src/main/java/com/example/api_rest_crud/services/ProductoServiceI.java
@@ -1,4 +1,18 @@
 package com.example.api_rest_crud.services;
 
-public class ProductoServiceI {
+import com.example.api_rest_crud.entities.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoServiceI {
+    List<Producto> buscarTodos();
+
+    Optional<Producto> buscarPorId(Long id);
+
+    Producto Guardar(Producto product);
+
+    Optional<Producto> actualizar(Long id, Producto product);
+
+    Optional<Producto> borrar(Long id);
 }
--- api_rest_crud/src/main/java/com/example/api_rest_crud/services/ProductoServiceImpl.java
+++ api_rest_crud/src/main/java/com/example/api_rest_crud/services/ProductoServiceImpl.java
@@ -1,4 +1,64 @@
 package com.example.api_rest_crud.services;
 
-public class ProductoServiceImpl {
+import com.example.api_rest_crud.entities.Producto;
+import com.example.api_rest_crud.repositories.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductoServiceImpl implements ProductoServiceI {
+
+    @Autowired
+    private ProductoRepository repository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Producto> buscarTodos() {
+        return repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Producto> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Producto Guardar(Producto nuevoProducto) {
+        return repository.save(nuevoProducto);
+    }
+
+    @Transactional
+    @Override
+    public Optional<Producto> actualizar(Long id, Producto producto) {
+
+        Optional<Producto> productoOptional = repository.findById(id);
+
+        if (productoOptional.isPresent()) {
+            Producto productoGuardado = productoOptional.orElseThrow();
+
+            productoGuardado.setNombre(producto.getNombre());
+            productoGuardado.setDescripcion(producto.getDescripcion());
+            productoGuardado.setPrecio(producto.getPrecio());
+            productoGuardado.setImagen(producto.getImagen());
+
+            return Optional.of(repository.save(productoGuardado));
+        }
+
+        return productoOptional;
+    }
+
+
+    @Transactional
+    @Override
+    public Optional<Producto> borrar(Long id) {
+        Optional<Producto> optionalProduct = repository.findById(id);
+        optionalProduct.ifPresent(repository::delete);
+        return optionalProduct;
+    }
 }
