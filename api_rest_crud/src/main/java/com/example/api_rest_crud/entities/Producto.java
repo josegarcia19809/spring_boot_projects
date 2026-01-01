4851ebd1c995f5b3ce5f17b3245c1105634bb08a
Jose Garcia <josegarcia19809@gmail.com>
2026-01-01 10:17:04 -0600

    API REST MySQL & Security Producto: Creando clases entity y repository

--- api_rest_crud/src/main/java/com/example/api_rest_crud/entities/Producto.java
+++ api_rest_crud/src/main/java/com/example/api_rest_crud/entities/Producto.java
@@ -1,4 +1,22 @@
 package com.example.api_rest_crud.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Integer precio;
+    private String descripcion;
+    private String imagen;
 }
--- api_rest_crud/src/main/java/com/example/api_rest_crud/repositories/ProductoRepository.java
+++ api_rest_crud/src/main/java/com/example/api_rest_crud/repositories/ProductoRepository.java
@@ -1,4 +1,9 @@
 package com.example.api_rest_crud.repositories;
 
-public class ProductoRepository {
+import com.example.api_rest_crud.entities.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
