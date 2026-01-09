14823c8e7e2a4d7b0185db45234bec8ab875da63
Jose Garcia <josegarcia19809@gmail.com>
2026-01-08 20:15:42 -0600

    API REST MySQL & Security Producto: Asignando roles y encriptando password con Bcrypt

--- api_rest_crud/src/main/java/com/example/api_rest_crud/services/UserServiceImpl.java
+++ api_rest_crud/src/main/java/com/example/api_rest_crud/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.api_rest_crud.services;
 
+import com.example.api_rest_crud.entities.Role;
 import com.example.api_rest_crud.entities.User;
 import com.example.api_rest_crud.repositories.RoleRepository;
 import com.example.api_rest_crud.repositories.UserRepository;
@@ -8,7 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -31,6 +34,15 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public User save(User user) {
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+        optionalRoleUser.ifPresent(roles::add);
+        if (user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
