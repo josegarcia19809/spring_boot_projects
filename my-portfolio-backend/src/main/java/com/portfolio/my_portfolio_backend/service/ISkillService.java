7605fc37b517ea462cca0bf835032409d7769ef5
Jose Garcia <josegarcia19809@gmail.com>
2026-08-27 22:27:31 -0600

    Skill: finalizando repositorio y servicio

---
+++ my-portfolio-backend/src/main/java/com/portfolio/my_portfolio_backend/service/ISkillService.java
@@ -0,0 +1,18 @@
+package com.portfolio.my_portfolio_backend.service;
+
+import com.portfolio.my_portfolio_backend.model.Skill;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ISkillService {
+    List<Skill> findAll();
+
+    Optional<Skill> findById(Long id);
+
+    Skill save(Skill skill);
+
+    void deleteById(Long id);
+
+    List<Skill> findSkillsByPersonalInfoId(Long personalInfoId);
+}
